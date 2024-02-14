1bcf675b05d2d74931aeb7303927877283b86874
bububu801 <157214620+bububu801@users.noreply.github.com>
2024-02-14 17:10:36 +0800

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,14 @@
+package No2;
+
+public class Vehicle {
+    public String program;
+    public String color;
+    public String speed;
+
+    public String wheels;
+
+    public void Vehicle(){
+
+    }
+
+}
