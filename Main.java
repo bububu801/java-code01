55b91eea180c79c692a90b32d0365a794cfeefa8
bububu801 <157214620+bububu801@users.noreply.github.com>
2024-02-14 17:05:41 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package No2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--------第一题--------");
+        Book book=new Book();
+        book.setbook("Java Programming","John Smith","2022");
+        book.displayInfo();
+        System.out.println("--------第二题--------");
+        Car car=new Car();
+        car.Car();
+        System.out.println("-----------------");
+        Truck truck=new Truck();
+        truck.Truck();
+        System.out.println("--------第三题--------");
+        Calcuator calcuator=new Calcuator();
+        calcuator.Calcuator();
+        System.out.println("--------第四题--------");
+        LongestCommonPrefix c=new LongestCommonPrefix();
+        c.LongestCommonPrefix();
+
+
+    }
+}
